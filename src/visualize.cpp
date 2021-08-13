82b0f820f6c8c397eb92c7ad1a55b43344883649
Shuo <shuoy@nvidia.com>
2021-08-12 22:09:46 -0400

    use LowStateStamped; listen to tf

--- src/visualize.cpp
+++ src/visualize.cpp
@@ -32,7 +32,7 @@
 #include "robot_markers/builder.h"
 
 // unitree message 
-#include <unitree_legged_msgs/LowState.h>
+#include <unitree_legged_msgs/LowStateStamped.h>
 
 
 // for visualize robot in rviz robomarker
@@ -42,26 +42,27 @@ ros::Publisher marker_arr_pub;
 
 // move joint position as global variable 
 std::map<std::string, double> joint_positions;
+tf::TransformListener* listener;
 
-void a1_state_callback(const unitree_legged_msgs::LowStateConstPtr& a1_state) 
+void a1_state_callback(const unitree_legged_msgs::LowStateStampedConstPtr& a1_state) 
 {
     // ROS_INFO("received");
     // FL 
-    joint_positions["FL_hip_joint"]   = a1_state -> motorState[0].q;
-    joint_positions["FL_thigh_joint"] = a1_state -> motorState[1].q;
-    joint_positions["FL_calf_joint"]  = a1_state -> motorState[2].q;
+    joint_positions["FL_hip_joint"]   = a1_state -> state.motorState[0].q;
+    joint_positions["FL_thigh_joint"] = a1_state -> state.motorState[1].q;
+    joint_positions["FL_calf_joint"]  = a1_state -> state.motorState[2].q;
     // FR 
-    joint_positions["FR_hip_joint"]   = a1_state -> motorState[3].q;
-    joint_positions["FR_thigh_joint"] = a1_state -> motorState[4].q;
-    joint_positions["FR_calf_joint"]  = a1_state -> motorState[5].q;
+    joint_positions["FR_hip_joint"]   = a1_state -> state.motorState[3].q;
+    joint_positions["FR_thigh_joint"] = a1_state -> state.motorState[4].q;
+    joint_positions["FR_calf_joint"]  = a1_state -> state.motorState[5].q;
     // RL 
-    joint_positions["RL_hip_joint"]   = a1_state -> motorState[6].q;
-    joint_positions["RL_thigh_joint"] = a1_state -> motorState[7].q;
-    joint_positions["RL_calf_joint"]  = a1_state -> motorState[8].q;
+    joint_positions["RL_hip_joint"]   = a1_state -> state.motorState[6].q;
+    joint_positions["RL_thigh_joint"] = a1_state -> state.motorState[7].q;
+    joint_positions["RL_calf_joint"]  = a1_state -> state.motorState[8].q;
     // RR 
-    joint_positions["RR_hip_joint"]   = a1_state -> motorState[9].q;
-    joint_positions["RR_thigh_joint"] = a1_state -> motorState[10].q;
-    joint_positions["RR_calf_joint"]  = a1_state -> motorState[11].q;
+    joint_positions["RR_hip_joint"]   = a1_state -> state.motorState[9].q;
+    joint_positions["RR_thigh_joint"] = a1_state -> state.motorState[10].q;
+    joint_positions["RR_calf_joint"]  = a1_state -> state.motorState[11].q;
 
     // ROS_INFO("build");
     builder->SetNamespace("a1_robot");
@@ -70,14 +71,22 @@ void a1_state_callback(const unitree_legged_msgs::LowStateConstPtr& a1_state)
     builder->Build(&a1_viz_array,1);
 
     // ROS_INFO("vis_body_pose");
+    // listen to tf
+    tf::StampedTransform transform;
     geometry_msgs::Pose vis_body_pose;
-    vis_body_pose.orientation.x = a1_state -> imu.quaternion[1];
-    vis_body_pose.orientation.y = a1_state -> imu.quaternion[2];
-    vis_body_pose.orientation.z = a1_state -> imu.quaternion[3];
-    vis_body_pose.orientation.w = a1_state -> imu.quaternion[0];  // notice this order   0:w, 1:x, 2:y, 3:z
-    vis_body_pose.position.x    = a1_state -> position.x; 
-    vis_body_pose.position.y    = a1_state -> position.y; 
-    vis_body_pose.position.z    = a1_state -> position.z;
+    try{
+        listener -> lookupTransform("/a1_world", "/a1_body", ros::Time(0), transform);    
+        vis_body_pose.orientation.x = transform.getRotation().getX();
+        vis_body_pose.orientation.y = transform.getRotation().getY();
+        vis_body_pose.orientation.z = transform.getRotation().getZ();
+        vis_body_pose.orientation.w = transform.getRotation().getW();  // notice this order   0:w, 1:x, 2:y, 3:z
+        vis_body_pose.position.x    = transform.getOrigin().x();
+        vis_body_pose.position.y    = transform.getOrigin().y();
+        vis_body_pose.position.z    = transform.getOrigin().z();
+    }
+    catch (tf::TransformException &ex) {
+      ROS_ERROR("%s",ex.what());
+    }
 
     // ROS_INFO("set");
     builder->SetPose(vis_body_pose);
@@ -93,7 +102,7 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "a1_visualizer");
     ros::NodeHandle nh;
     ros::Rate loop_rate(100);
-
+    listener = new tf::TransformListener();
     // the robot must publish a message with type unitree_legged_msgs::LowState
     ros::Subscriber sub_a1_msg = nh.subscribe("/a1/debug_state", 1000, a1_state_callback);
 
